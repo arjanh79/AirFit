4effcef05ef10177c80022f3faee5a2a683a8925
arjanh79 <arjan_heertjes@ziggo.nl>
2025-03-14 18:43:32 +0100

    Workout + Estimate intensity.

--- v0/AI/update_model.py
+++ v0/AI/update_model.py
@@ -6,7 +6,6 @@ from dev.v0.AI.train import ModelTraining
 pre = WorkoutPreprocessor()
 dataset = WorkoutDataset(pre.embeddings_x, pre.data_x, pre.data_y, pre.weighted_loss)
 
-
 mt = ModelTraining(AirFitDNN(), dataset)
 
 mt.train_model()
--- v0/WO/generate_workout.py
+++ v0/WO/generate_workout.py
@@ -2,17 +2,53 @@ import numpy as np
 import pandas as pd
 import torch
 
+from dev.v0.AI.dataset import WorkoutDataset
 from dev.v0.AI.modelDNN import AirFitDNN
+from dev.v0.AI.train import ModelTraining
 from dev.v0.DB.factories import RepositoryFactory
 
 
-class NewWorkOut:
+class NewWorkout:
     def __init__(self):
         self.repo = RepositoryFactory.get_repository('sqlite')
         self.all_exercises = self.get_all_exercises()
         self.rnd_gen = np.random.default_rng()
         self.model = self.get_model()
-        self.get_mappings = self.get_mappings()
+        self.mappings = self.get_mappings()
+
+        self.warming_up = self.get_warming_up()
+        self.finale = self.get_finale()
+        self.core = self.get_core()
+
+        self.workout = self.get_workout()
+        print(self.workout)
+
+        self.estimate_intensity()
+
+    def estimate_intensity(self):
+        X_embeddings, X_features = self.workout.iloc[:, 0], self.workout.iloc[:, 1:]
+        X_embeddings = torch.tensor(X_embeddings.values).long().reshape((1, 20))
+        X_features = torch.tensor(X_features.values, dtype=torch.float32).reshape((1, 60))
+        y = torch.tensor([[3.5]])
+        wl = torch.tensor([[1]])
+
+        dataset = WorkoutDataset(X_embeddings, X_features, y, wl)
+        mt = ModelTraining(AirFitDNN(), dataset)
+        print(f'\nExpected Intensity: {mt.eval_workout().item():.3f}')
+
+
+
+    def get_workout(self):
+        workout = pd.concat([self.warming_up, self.core, self.finale], ignore_index=True)
+        workout['reps'] = np.where(workout['name'].str.contains('Plank'), 45, 10)
+
+        workout = workout.reindex(range(20), fill_value=0)
+        workout.loc[workout['name'] == 0, 'name'] = 'UNK'
+        workout['seq_num'] = range(1, 20+1)
+        workout['name'] = workout['name'].map(self.mappings).astype(int)
+
+        return workout
+
 
     def get_mappings(self):
         mappings = self.repo.get_mapping()[0]
@@ -35,30 +71,40 @@ class NewWorkOut:
         min_exercises = min_exercises[~min_exercises['name'].str.contains('Plank|Clean|Push')]
 
         exercises = self.rnd_gen.choice(len(min_exercises), size=3, replace=False)
-        exercises = min_exercises.iloc[exercises, :].reset_index(drop=True)
-        exercises['reps'] = 10
+        exercises = min_exercises.iloc[exercises, :]
 
         return exercises
 
-    def get_final(self):
+    def get_finale(self):
         order = ['Push Ups', 'Squats', 'Clean and Press']
         exercises = pd.DataFrame(self.all_exercises[0], columns=self.all_exercises[1])
         single_weight = exercises.loc[exercises['name'].isin(['Push Ups', 'Clean and Press'])]
 
-        finale = pd.DataFrame(self.rnd_gen.choice(exercises[exercises['name'] == 'Squats'], size=1), columns=['name','weight'])
-        finale = pd.concat((finale, single_weight), ignore_index=True)
+        finale = exercises[exercises['name'] == 'Squats'].sample(n=1)
+        finale = pd.concat((finale, single_weight))
         finale['name'] = pd.Categorical(finale['name'], categories=order, ordered=True)
-        finale = finale.sort_values('name').reset_index(drop=True)
-        finale['reps'] = 10
+        finale = finale.sort_values('name')
 
         return finale
 
     def get_core(self):
+        core1 = self.get_core_element([self.warming_up])
+        core3 = self.get_core_element([self.finale])
+        core2 = self.get_core_element([core1, core3])
+        core = pd.concat([core1, core2, core3], ignore_index=True)
+        return core
+
+    def get_core_element(self, exclude):
         exercises = pd.DataFrame(self.all_exercises[0], columns=self.all_exercises[1])
-        print(exercises)
+        exclude = pd.concat(exclude)
+        available = exercises.loc[~exercises.index.isin(exclude.index)]
+        diff_ex = 0
+
+        while diff_ex < 3:
+            core = available.sample(n=3, replace=False)
+            grouped = core.groupby(['name']).count()
+            diff_ex = grouped.shape[0]
+        return core
 
 
-nwo = NewWorkOut()
-print(nwo.get_warming_up())
-print(nwo.get_final())
-nwo.get_core()
+nwo = NewWorkout()
