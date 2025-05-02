d72801ec52798283042469fde1dccc2f28093136
arjanh79 <arjan_heertjes@ziggo.nl>
2025-05-02 13:04:13 +0200

    Changed 'random' workout to prefer 'classic'

--- v0/WO/WorkoutFactory.py
+++ v0/WO/WorkoutFactory.py
@@ -5,7 +5,6 @@ from dev.v0.WO.ClassicWorkout import ClassicWorkout
 from dev.v0.WO.FocusWorkout import FocusWorkout
 from dev.v0.WO.ForgeWorkout import ForgeWorkout
 
-
 def workout_factory(workout_type):
 
     type_list = ['random', 'classic', 'focus', 'forge']
@@ -16,11 +15,8 @@ def workout_factory(workout_type):
     print(f'Workout type: {workout_type}')
 
     if workout_type == 'random':
-        prob = [1] * len(type_list[1:])
-        prob[0] += len(type_list[2:])
-
         rnd_gen = np.random.default_rng()
-        workout_type = rnd_gen.choice(type_list[1:], 1, p=[0.5, 0.25, 0.25])
+        workout_type = rnd_gen.choice(type_list[1:], 1, p=[0.4, 0.3, 0.3])
         print(f'Workout type update: {workout_type[0]}')
 
     if workout_type == 'classic':
@@ -29,6 +25,5 @@ def workout_factory(workout_type):
         return FocusWorkout()
     if workout_type == 'forge':
         return ForgeWorkout()
-
 
     return None
