39e39a19ca08edb2ec2e584a8cccfb9e7df3a9d7
Arjan <7210597+arjanh79@users.noreply.github.com>
2025-06-11 21:21:14 +0200

    .

--- v1/WO/CoreWorkout.py
+++ v1/WO/CoreWorkout.py
@@ -22,18 +22,23 @@ class CoreWorkout(BasicWorkout):
 
     def get_core(self, warming_up, finale):
         df = pd.DataFrame(self.all_exercises[0], columns=self.all_exercises[1])
-        e1 = df[df['name'] == 'Dead Bug - Static'].sample(n=1)
-        e2 = df[df['name'] == 'Plank'].sample(n=1)
-        e3 = df[df['name'] == 'Shoulder Taps'].sample(n=1)
+        e1 = df[df['name'] == 'Bosu Clean and Press'].sample(n=1)
+        e2 = df[df['name'] == 'Dead Bug - Static'].sample(n=1)
+        e3 = df[df['name'] == 'Plank'].sample(n=1)
 
-        e4 = df[df['name'] == 'Reverse Fly'].sample(n=1)
-        e5 = df[df['name'] == 'Bent-Over Row'].sample(n=1)
-        e6 = df[df['name'] == 'Squats'].sample(n=1)
+        e4 = df[df['name'] == 'Shoulder Taps'].sample(n=1)
+        e5 = df[df['name'] == 'Squats'].sample(n=1)
+        e6 = df[df['name'] == 'Push Ups'].sample(n=1)
 
         e7 = df[df['name'] == 'Step Ups'].sample(n=1)
-        e8 = df[df['name'] == 'Bosu Plank'].sample(n=1)
-        e9 = df[df['name'] == 'Push Ups'].sample(n=1)
+        e8 = df[df['name'] == 'Deadlift'].sample(n=1)
+        e9 = df[df['name'] == 'Bent-Over Row'].sample(n=1)
+
+        e10 = df[df['name'] == 'Reverse Fly'].sample(n=1)
+        e11 = df[df['name'] == 'Clean and Press'].sample(n=1)
+        e12 = df[df['name'] == 'Bosu Plank'].sample(n=1)
 
 
-        result = pd.concat([e1, e2, e3, e4, e5, e6, e7, e8, e9], ignore_index=True)
+
+        result = pd.concat([e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12], ignore_index=True)
         return result
