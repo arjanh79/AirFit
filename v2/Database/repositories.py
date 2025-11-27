e185579e0540f676f741b6752610de4d8980020f
arjan <arjan_heertjes@ziggo.nl>
2025-11-27 18:30:20 +0100

    Generate basic workout.

--- v2/Database/repositories.py
+++ v2/Database/repositories.py
@@ -10,7 +10,37 @@ class GenericRepository:
         sql = 'SELECT exercise_id FROM BlockExercise WHERE block_id = ? ORDER BY seq;'
         return self.db.execute_query(sql, (block_id, ))
 
+    def get_exercises(self, exercise_ids):
+        params = ','.join(['?' for _ in exercise_ids])
 
+        sql = (f'SELECT EW.exercise_id, EW.weight_id FROM ExerciseWeight EW '
+               f'JOIN Weight W on EW.weight_id = W.weight_id '
+               f'JOIN Exercise E on EW.exercise_id = E.exercise_id '
+               f'WHERE EW.exercise_id IN ({params}) '
+               f'GROUP BY EW.exercise_id, EW.weight_id')
+
+        return self.db.execute_query(sql, exercise_ids)
+
+    def get_workout_compose(self, block_id, where_pairs):
+
+        conditions = " OR ".join(
+            "(BE.exercise_id = ? AND EW.weight_id = ?)"
+            for _ in where_pairs
+        )
+
+        sql = (f'SELECT B.block_id, B.core, BE.seq, BE.exercise_id, E.default_value, W.weight_id, EQ.equipment_id '
+               f'FROM BlockExercise BE '
+               f'JOIN Block B ON BE.block_id = B.block_id '
+               f'JOIN Exercise E ON BE.exercise_id = E.exercise_id '
+               f'JOIN ExerciseWeight EW ON BE.exercise_id = EW.exercise_id '
+               f'JOIN Weight W ON EW.weight_id = W.weight_id '
+               f'JOIN Equipment EQ on W.equipment_id = EQ.equipment_id '
+               f'WHERE B.block_id = ? AND ({conditions}) ORDER BY seq')
+
+        where_pairs = [item for i in where_pairs for item in i]
+        where_pairs = [block_id] + where_pairs
+
+        return self.db.execute_query(sql,where_pairs)
 
 
 class SQLiteRepository(GenericRepository):
--- v2/Workout/WorkoutGenerator.py
+++ v2/Workout/WorkoutGenerator.py
@@ -1,8 +1,10 @@
 import random
 from dataclasses import dataclass
+from collections import defaultdict, Counter
 
 from v2.Database.factories import RepositoryFactory
 
+import pandas as pd
 
 @dataclass
 class Block:
@@ -10,15 +12,36 @@ class Block:
     block_id: int
     exercises: list[int]
 
+@dataclass
+class Weight:
+    block_type: int
+    weights: dict[int, int]
+
 
 class WorkoutGenerator:
     def __init__(self):
         self.repo = RepositoryFactory.get_repository('sqlite')
         self.workout = self.get_blocks()
-        print(self.workout[0])
-        print(self.workout[1])
+        self.weights = self.get_exercise_weights()
 
-    def get_block(self, block_type: int):
+        self.complete_workout = self.compose()
+
+        print(self.complete_workout)
+
+    def compose(self):
+        combined_workout = []
+
+        for i in [0, 1]:
+            block_id = self.workout[i].block_id
+            where_pairs = list(self.weights[i].weights.items())
+            result = self.repo.get_workout_compose(block_id, where_pairs)
+            df = pd.DataFrame(data=result[0], columns=result[1])
+            combined_workout.append(df)
+
+        return pd.concat(combined_workout, ignore_index=True)
+
+
+    def get_block(self, block_type):
         block_ids, _ = self.repo.get_all_blocks(block_type)
         block_id = random.choice(block_ids)[0]
         block, _ = self.repo.get_block(block_id)
@@ -31,6 +54,49 @@ class WorkoutGenerator:
             self.get_block(0),
             self.get_block(1)
         ]
+
+
+    def get_exercise_weights(self):
+        return [
+            self.get_exercise_weight(0),
+            self.get_exercise_weight(1),
+        ]
+
+    def get_exercise_weight(self, block_type):
+
+        exercise_ids = self.workout[block_type].exercises
+
+        result, _ = self.repo.get_exercises(exercise_ids)
+        weights = defaultdict(set)
+
+
+        for exercise, weight in result:
+            weights[exercise].add(weight)
+
+        while True:
+            freq = Counter()
+            for weight_set in (v for v in weights.values() if len(v) > 1):
+                for w in weight_set:
+                    freq[w] += 1
+
+            if not freq:
+                break
+
+            max_count = max(freq.values())
+            selected_weight = random.choice([w for w, c in freq.items() if c == max_count])
+
+            for ex_id, weight_set in weights.items():
+                if len(weight_set) > 1 and selected_weight in weight_set:
+                    weights[ex_id] = {selected_weight}
+
+        for ex_id, weight_set in weights.items():
+            if len(weight_set) > 1:
+                weights[ex_id] = {random.choice(list(weight_set))}
+
+        weights = {k: v.pop() for k, v in weights.items()}
+        return Weight(block_type, weights)
+
+
 
 
 wg = WorkoutGenerator()
