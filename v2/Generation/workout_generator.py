8370d207595a4bbe5c206bcd7effe91ee2719fc3
arjan <arjan_heertjes@ziggo.nl>
2025-12-23 17:41:13 +0100

    Also saving the exercise_ids as seen during training.

--- v2/Generation/workout_generator.py
+++ v2/Generation/workout_generator.py
@@ -8,45 +8,6 @@ class WorkoutGenerator:
     def __init__(self):
         self.repo = RepositoryFactory.get_repository('sqlite')
 
-        self.trainer = WorkoutTrainer()
-        self.train_model(epochs=500)
-
         self.workout_token = [1]
         self.workout_exercise = []
 
-        self.get_workout()
-
-        self.eid_mappings = self._get_eid_mappings()
-        self.get_exercise_names()
-
-    def get_exercise_names(self):
-        exercise_names = [self.eid_mappings[eid] for eid in self.workout_exercise]
-        print(exercise_names)
-
-
-
-    def _get_eid_mappings(self):
-        data, cols = self.repo.get_exercise_ids()
-        eid_mappings = {k: v for k, v in data}
-        return eid_mappings
-
-
-
-    def train_model(self, epochs: int=100) -> None:
-        for epoch in range(epochs):
-            print(f'Epoch {epoch+1}')
-            self.trainer.train_one_epoch()
-
-
-    def get_workout(self):
-        for _ in range(12):
-            token, exercise = self.trainer.predict_next(self.workout_token)
-            self.workout_token.append(token)
-            self.workout_exercise.append(exercise)
-        print(f'Workout (tokens): {self.workout_token}')
-        print(f'Workout (e_ids) :    {self.workout_exercise}')
-
-
-
-
-wg  = WorkoutGenerator()
--- v2/Trainer/train.py
+++ v2/Trainer/train.py
@@ -1,3 +1,4 @@
+from v2.Data.factories import RepositoryFactory
 from v2.Trainer.trainer import WorkoutTrainer
 from v2.config import MODEL_PATH
 from v2.Domain.workout_combinator import WorkoutCombinator
@@ -8,22 +9,27 @@ import json
 
 def main() -> None:
 
+    repo = RepositoryFactory.get_repository('sqlite')
+
     combinator = WorkoutCombinator()
     workouts = combinator.create_workouts()
     ds = WorkoutDataset(workouts)
 
-    save_token_mappings('ex_to_id.json', ds.ex_to_id)
-    save_token_mappings('id_to_ex.json', ds.id_to_ex)
+    exercise_mappings, _ = repo.get_exercise_ids()
+    exercise_mappings = {k: v for k, v in exercise_mappings}
+
+    save_data('ex_to_id.json', ds.ex_to_id)
+    save_data('id_to_ex.json', ds.id_to_ex)
+    save_data('exercise_ids.json', exercise_mappings)
 
     trainer = WorkoutTrainer(combinator, ds)
-    trainer.fit(epochs=50)
+    trainer.fit(epochs=5)
     trainer.save_model()
 
 
-def save_token_mappings(filename: str, data: dict) -> None:
+def save_data(filename: str, data: dict) -> None:
     with open(MODEL_PATH / filename, 'w') as f:
-        json.dump(data, f)
-
+        json.dump(data, f, indent=2)
 
 
 if __name__ == "__main__":
