- intensity_generator: If gradients fall too low, finetuning on the number of reps will stop. - intensity_round: Aligned the lower intensity bound with intensity_generator. Correct value is 4.25.

v2/Generation/intensity_generator.py
@@ -80,7 +80,7 @@ class IntensityGenerator:
 
         loss_fn = torch.nn.MSELoss()
 
-        for step in range(250 + 1):
+        for step in range(500 + 1):
             optimizer.zero_grad()
 
             x_work = x.clone()
@@ -89,11 +89,19 @@ class IntensityGenerator:
             out = self.model(x_work).squeeze()
 
             if 4.5 <= out.item() <= 4.75:  # Good enough
-                print(f'step {step:04d}: intensity={out.item():.5f}')
+                print(f'[INTENSITY] step {step:04d}: intensity={out.item():.5f}')
                 break
 
             loss = loss_fn(out, target)
             loss.backward()
+
+            with torch.no_grad():
+                grad_abs_max = reps.grad.abs().max().item()
+                if grad_abs_max < 0.03:
+                    print(f'[GRADIENT] step {step:04d}: intensity={out.item():.5f}')
+                    break
+
+
 
             optimizer.step()
 